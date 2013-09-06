b293f8c27360bbb208dd1e989ec760156808c2dc
drhenner <drhenner@yahoo.com>
2013-09-06 16:55:55 -0700

    use will paginate

--- lib/generators/nifty/scaffold/templates/actions/index.rb
+++ lib/generators/nifty/scaffold/templates/actions/index.rb
@@ -1,4 +1,4 @@
   def index
     @<%= instances_name %> = <%= class_name %>.order(sort_column + " " + sort_direction).
-                                              page(pagination_page).per(pagination_rows)
+                                              paginate(page: pagination_page, per_page: pagination_rows)
   end
