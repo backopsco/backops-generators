4770bbed5fed0868e4df3d98d15b83204d302216
drhenner <drhenner@yahoo.com>
2013-09-11 17:21:25 -0700

    update specs to add sign_in

--- lib/generators/nifty/scaffold/templates/tests/rspec/controller.rb
+++ lib/generators/nifty/scaffold/templates/tests/rspec/controller.rb
@@ -3,5 +3,11 @@ require  'spec_helper'
 describe <%= plural_class_name %>Controller do
   render_views
 
+  before do
+    controller.stub(:user_has_permission?) { true }
+    @user = FactoryGirl.create(:controller)
+    sign_in @user
+  end
+
   <%= controller_methods 'tests/rspec/actions' %>
 end
