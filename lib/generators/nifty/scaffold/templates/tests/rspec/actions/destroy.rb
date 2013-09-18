2a8bceeb439d42fda40977379ab6c1a9a8dee467
drhenner <drhenner@yahoo.com>
2013-09-18 11:30:30 -0700

    update generator to use expect rather than "should" in the tests

--- lib/generators/nifty/scaffold/templates/tests/rspec/actions/destroy.rb
+++ lib/generators/nifty/scaffold/templates/tests/rspec/actions/destroy.rb
@@ -2,16 +2,16 @@
     <%= instance_name %> = FactoryGirl.create(:<%= instance_name %>)
     delete :destroy, id: <%= instance_name %>.id
     expect(response).to redirect_to(<%= items_url %>)
-    <%= class_name %>.exists?(<%= instance_name %>.id).should be_false
-    <%= class_name %>.find(<%= instance_name %>.id).active.should be_false
+    expect(<%= class_name %>.exists?(<%= instance_name %>.id)).to be_false
+    expect(<%= class_name %>.find(<%= instance_name %>.id).active).to be_false
   end
 
 <% if view_language != 'jbuilder' %>
   it 'destroy action should destroy model and redirect to index action' do
     <%= instance_name %> = FactoryGirl.create(:<%= instance_name %>)
     delete :destroy, id: <%= instance_name %>.id, format: :json
-    <%= class_name %>.exists?(<%= instance_name %>.id).should be_false
-    <%= class_name %>.find(<%= instance_name %>.id).active.should be_false
+    expect(<%= class_name %>.exists?(<%= instance_name %>.id)).to be_false
+    expect(<%= class_name %>.find(<%= instance_name %>.id).active).to be_false
     expect(response.status.to_i).to eq 200
   end
 <% end %>
