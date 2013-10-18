cbcb9f2a84c029a777cf166d1d2b62925988c0e0
drhenner <drhenner@yahoo.com>
2013-10-17 17:05:21 -0700

    syntax error

--- lib/generators/nifty/scaffold/templates/actions/index.rb
+++ lib/generators/nifty/scaffold/templates/actions/index.rb
@@ -7,7 +7,6 @@
     end
   end
 <%- else -%>
-<%- unless view_language == 'ember_api' -%>
   def index
     @<%= instances_name %> = <%= class_name %>.order(sort_column + " " + sort_direction)
                                               .paginate(page: pagination_page, per_page: pagination_rows)
